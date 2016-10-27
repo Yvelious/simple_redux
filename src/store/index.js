4f2fda9e925bbe8d2573560e564f003ac31a4014
Stanislav Le <stanislav.le@onyxenterprises.com>
2016-10-27 10:26:14 +0300

    bigin redux do

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,8 @@
 /* global NODE_ENV */
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import reducer from '../reducer/index';
-import thunk from 'redux-thunk';
 
-let composeFuncList = [applyMiddleware(/*callAPI, */thunk)];
 
-if (typeof NODE_ENV === 'string' && NODE_ENV === 'development') {
-	composeFuncList.push(window.devToolsExtension ? window.devToolsExtension() : f => f);
-}
-
-const enhancer = compose(
-	...composeFuncList 
-);
-
-const store = createStore(reducer, {}, enhancer);
+const store = createStore(reducer, {});
 
 export default store;    
